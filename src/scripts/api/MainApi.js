51ab8384ed64808211c65d42383f73f807f8e6ba
Rim Sungatovich <rsungatovichsocial@gmail.com>
2020-12-01 13:41:57 +0300

    added main-api class

--- src/scripts/api/MainApi.js
+++ src/scripts/api/MainApi.js
@@ -1,28 +1,77 @@
 export default class MainApi {
-  constructor() {
+  constructor(props) {
+    this.baseUrl = props.baseUrl;
+    this.headers = props.headers;
   }
 
-  signup() {
-
+  _getResolve = (mes, res) => {
+    if (res.ok) return res.json();
+    return Promise.reject(`${mes} ${res.status}`);
   }
 
-  signin() {
+  signup(body) {
+    return fetch(`${this.baseUrl}/signup`, {
+      method: 'POST',
+      headers: this.headers,
+      credentials: 'same-origin',
+      body: JSON.stringify(body),
+    })
+      .then((res) => this._getResolve('', res));
+  }
 
+  signin(body) {
+    return fetch(`${this.baseUrl}/signin`, {
+      method: 'POST',
+      headers: this.headers,
+      credentials: 'same-origin',
+      body: JSON.stringify(body),
+    })
+      .then((res) => this._getResolve('', res));
+  }
+
+  logout() {
+    return fetch(`${this.baseUrl}/logout`, {
+      method: 'POST',
+      headers: this.headers,
+      credentials: 'same-origin',
+    })
+      .then((res) => this._getResolve('', res));
   }
 
   getUserData() {
-
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'GET',
+      headers: this.headers,
+      credentials: 'same-origin',
+    })
+      .then((res) => this._getResolve('', res));
   }
 
   getArticles() {
-
+    return fetch(`${this.baseUrl}/articles`, {
+      method: 'GET',
+      headers: this.headers,
+      credentials: 'same-origin',
+    })
+      .then((res) => this._getResolve('', res));
   }
 
-  createArticle() {
-
+  createArticle(body) {
+    return fetch(`${this.baseUrl}/articles`, {
+      method: 'POST',
+      headers: this.headers,
+      credentials: 'same-origin',
+      body: JSON.stringify(body),
+    })
+      .then((res) => this._getResolve('', res));
   }
 
-  removeArticle() {
-
+  removeArticle(id) {
+    return fetch(`${this.baseUrl}/articles/${id}`, {
+      method: 'DELETE',
+      headers: this.headers,
+      credentials: 'same-origin',
+    })
+      .then((res) => this._getResolve('', res));
   }
 }
